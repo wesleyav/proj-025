7b1e77eb7473228ec27dd15e795485abe80a9270
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-07 18:18:35 -0300

    Adiciona classe Book

---
+++ src/main/java/app/entities/Book.java
@@ -0,0 +1,68 @@
+package app.entities;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class Book implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	@JsonIgnore
+	private Author author;
+
+	public Book() {
+	}
+
+	public Book(Long id, String title, Author author) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+}
