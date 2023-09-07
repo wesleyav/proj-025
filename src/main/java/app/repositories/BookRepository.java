b45487b7a5ca8dbab775a0d0dd45fde77216b830
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-07 18:19:34 -0300

    Adiciona interface BookRepository

---
+++ src/main/java/app/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import app.entities.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
